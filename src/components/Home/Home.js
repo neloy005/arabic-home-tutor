af45bec3894c6d6c602d62971f9c512a6a6a05a7
neloy005 <arifistiekofficial@gmail.com>
2022-04-16 23:30:33 +0600

    verse section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,19 +7,35 @@ const Home = () => {
     return (
         <div>
             <h2>Arabic home tutor</h2>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h2>Learn Al Quran</h2>
-                        <p>And open up your way to jannah.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <div className='banner-container'>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={banner}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h2>Learn Al Quran</h2>
+                            <p>And open up your way to jannah.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+
+
+
+            <div>
+                <h1>Read a single verse everyday</h1>
+                <div className='verse-container'>
+                    <hr className='horizontal-line' />
+                    <h2>" تَبَـٰرَكَ ٱلَّذِى بِيَدِهِ ٱلْمُلْكُ وَهُوَ عَلَىٰ كُلِّ شَىْءٍۢ قَدِيرٌ "</h2>
+                    <hr />
+                    <h3>Meaning: Blessed is the One in Whose Hands rests all authority. And He is Most Capable of everything. (67:1)</h3>
+                    <hr className='horizontal-line' />
+                </div>
+
+            </div>
         </div>
     );
 };
