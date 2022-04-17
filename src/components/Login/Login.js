9b6174b38bc1fe225889f09de4a0f9791e5706d3
neloy005 <arifistiekofficial@gmail.com>
2022-04-17 23:51:54 +0600

    reset email done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import React, { useRef } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const emailRef = useRef('');
     const navigate = useNavigate()
     let errorMsg;
     const navigateToRegister = () => {
@@ -16,6 +17,11 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
+    const [
+        sendPasswordResetEmail,
+        sending
+    ] = useSendPasswordResetEmail(auth);
 
     if (user) {
         navigate('/home');
@@ -31,17 +37,24 @@ const Login = () => {
 
         signInWithEmailAndPassword(email, password);
     }
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+
+        await sendPasswordResetEmail(email);
+        console.log('email sent');
+    }
     return (
         <div className='login-container'>
             <h2>Login to your account</h2>
             <h3>Login to your personal account to start courses 😊</h3>
             <hr />
             <form onSubmit={handleSubmit}>
-                <input type="email" name='email' placeholder='Enter Your Email' required /> <br />
+                <input type="email" ref={emailRef} name='email' placeholder='Enter Your Email' required /> <br />
                 <input type="password" name='password' placeholder='Enter Your password' required /> <br />
                 {errorMsg}
                 <input className='submit-btn' type="submit" value='Login' />
             </form>
+            <p>Forget password? <span style={{ 'color': 'blue', 'cursor': 'pointer' }} onClick={resetPassword}>Reset Password</span></p>
             <p>New in my website? <span style={{ 'color': 'blue', 'cursor': 'pointer' }} onClick={navigateToRegister}>Please Register.</span></p>
         </div>
     );
